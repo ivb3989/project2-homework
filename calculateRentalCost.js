c6ac127452e813ec8241f5812613cfa3640d6f6e
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-02-01 19:53:07 +0200

    replaced ternary operator

--- calculateRentalCost.js
+++ calculateRentalCost.js
@@ -1,3 +1,15 @@
 export const calculateRentalCost = (days) => {
-    return days < 3 ? days*40 : days >= 7 ? days*40-50 : days*40-20;
-}
+  const price = 40;
+  const total = days * price;
+  const saleFor3AndMore = 20;
+  const saleFor7AndMore = 50;
+
+  if (days < 3) {
+    return total;
+  } else if (days >= 3 && days < 7) {
+    return total - saleFor3AndMore;
+  }
+
+  return total - saleFor7AndMore;
+ 
+};
