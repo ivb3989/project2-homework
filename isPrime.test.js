1bf2d6d6eb5325226245030e57b237a28ef6db73
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-02-01 19:55:35 +0200

    rewrote test

--- isPrime.test.js
+++ isPrime.test.js
@@ -1,19 +1,24 @@
-import { isPrime } from "./isPrime";
+import { isPrime } from './isPrime';
 
-describe("isPrime function", () => {
-  it("should return true when pass 2", () => {
+describe('isPrime function', () => {
+  it('should return true when pass 2', () => {
     expect(isPrime(2)).toBe(true);
   });
-  it("should return true when pass 73", () => {
-    expect(isPrime(73)).toBe(true);
-  });
-  it("should return true when pass 1", () => {
-    expect(isPrime(1)).toBe(false);
-  });
-  it("should return true when pass 75", () => {
+
+  it('should return true when pass 75', () => {
     expect(isPrime(75)).toBe(false);
   });
-  it("should return true when pass -1", () => {
+
+  it('should return true when pass 1', () => {
+    expect(isPrime(1)).toBe(false);
+  });
+
+  it('should return true when pass 75', () => {
+    expect(isPrime(75)).toBe(false);
+  });
+
+  it('should return true when pass -1', () => {
     expect(isPrime(-1)).toBe(false);
   });
 });
+
