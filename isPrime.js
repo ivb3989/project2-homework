c87c9ab1a8d8f1e7c7b0d8d61e0f98614f67fc77
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-02-01 19:54:50 +0200

    corrected mistakes

--- isPrime.js
+++ isPrime.js
@@ -1,6 +1,12 @@
 export const isPrime = (num) => {
-    if (isNaN(num) || num % 1 !== 0) return false;
-    num = Math.abs(num);
-    if (num === 0 || num === 1) return false;
-    return [...Array(num - 2)].map((_, i) => i + 2).filter((i) => num % i == 0).length == 0;
-}
+  if (isNaN(num) || num % 1 !== 0) {
+    return false;
+  }
+  num = Math.abs(num);
+
+  if (num === 0 || num === 1) {
+    return false;
+  }
+
+  return [...Array(num - 2)].map((_, element) => element + 2).filter((element) => num % element === 0).length === 0;
+};
