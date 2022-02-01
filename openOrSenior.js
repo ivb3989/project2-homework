9c3ee023ed7567bd721e2fcc4a6f142cc9190630
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-02-01 20:00:56 +0200

    corrected mistakes

--- openOrSenior.js
+++ openOrSenior.js
@@ -1,5 +1,8 @@
 export const openOrSenior = (list) => {
+  const appropriateAge = 40;
+  const workExperience = 15;
+
   return list.map((element) =>
-    element[0] >= 40 && element[1] > 15 ? "Senior" : "Open"
+    element[0] >= appropriateAge && element[1] > workExperience ? 'Senior' : 'Open',
   );
 };
